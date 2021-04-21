b073e3216251ae28e72830fa1c63fa41387e91a6
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-04-22 08:27:23 +0900

    feat: update crossfade sample

--- crossfade/src/main/kotlin/main.kt
+++ crossfade/src/main/kotlin/main.kt
@@ -1,5 +1,3 @@
-import androidx.compose.animation.Crossfade
-import androidx.compose.animation.core.tween
 import androidx.compose.desktop.Window
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -9,10 +7,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -20,38 +15,52 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-fun main() = Window {
-    var selectedPageName by remember { mutableStateOf("ONE") }
-    MaterialTheme {
-        Crossfade(
-            current = selectedPageName,
-            modifier = Modifier.fillMaxSize(),
-            animation = tween(1000)
-        ) { pageName ->
-            when (pageName) {
-                "ONE" -> {
-                    Box(modifier = Modifier.fillMaxSize().background(Color.Black)) {
-                        Text("1ページ目", style = TextStyle(fontSize = 32.sp, color = Color.White))
-                        Button(
-                            onClick = { selectedPageName = "TWO" },
-                            modifier = Modifier.width(200.dp).height(100.dp).align(Alignment.Center)
-                        ) {
-                            Text("2ページ目に遷移する")
-                        }
-                    }
-                }
-                "TWO" -> {
-                    Box(modifier = Modifier.fillMaxSize().background(Color.White)) {
-                        Text("2ページ目", style = TextStyle(fontSize = 32.sp, color = Color.Black))
-                        Button(
-                            onClick = { selectedPageName = "ONE" },
-                            modifier = Modifier.width(200.dp).height(100.dp).align(Alignment.Center)
-                        ) {
-                            Text("1ページ目に遷移する")
-                        }
-                    }
-                }
+const val ONE_PAGE_NAME = "ONE"
+const val TWO_PAGE_NAME = "TWO"
+
+fun main() {
+    Window("CROSSFADE") {
+        var selectedPageName by remember { mutableStateOf("ONE") }
+
+        fun navigateOnePage() {
+            selectedPageName = ONE_PAGE_NAME
+        }
+
+        fun navigateTwoPage() {
+            selectedPageName = TWO_PAGE_NAME
+        }
+
+        MaterialTheme {
+            when (selectedPageName) {
+                ONE_PAGE_NAME -> OnePage(onNavigate = { navigateTwoPage() })
+                TWO_PAGE_NAME -> TwoPage(onNavigate = { navigateOnePage() })
             }
+        }
+    }
+}
+
+@Composable
+fun OnePage(onNavigate: () -> Unit) {
+    Box(modifier = Modifier.fillMaxSize().background(Color.Black)) {
+        Text("1ページ目", style = TextStyle(fontSize = 32.sp, color = Color.White))
+        Button(
+            onClick = onNavigate,
+            modifier = Modifier.width(200.dp).height(100.dp).align(Alignment.Center)
+        ) {
+            Text("2ページ目に遷移する")
+        }
+    }
+}
+
+@Composable
+fun TwoPage(onNavigate: () -> Unit) {
+    Box(modifier = Modifier.fillMaxSize().background(Color.White)) {
+        Text("2ページ目", style = TextStyle(fontSize = 32.sp, color = Color.Black))
+        Button(
+            onClick = onNavigate,
+            modifier = Modifier.width(200.dp).height(100.dp).align(Alignment.Center)
+        ) {
+            Text("1ページ目に遷移する")
         }
     }
 }
